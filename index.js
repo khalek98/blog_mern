b7ac8a3f73504ee7aed189eb2a33402e0cf85b7b
Khalek <xalek.islam@gmail.com>
2022-08-14 15:34:05 +0300

    auth setting; connected libs: bcrypt, express, express-validator, jsonwebtoken, mongoose, nodemon

---
+++ index.js
@@ -0,0 +1,121 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+import userModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:17supusaH@cluster0.ufyxye3.mongodb.net/blog?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'wrong login or password',
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Wrong lodin or password',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(error);
+    res.status(500).json({
+      message: 'failed to sign in',
+    });
+  }
+});
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new userModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'failed to sign up',
+    });
+  }
+});
+
+app.get('/auth/me', checkAuth, (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+})
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK!');
+});
